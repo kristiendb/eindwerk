6ad74d7095e0617821da731eb73bb705055e7d30
Kristien De Boeck <deboeck_kristien@hotmail.com>
2024-05-15 14:30:43 +0200

    update

--- src/app/home/agenda/page.jsx
+++ src/app/home/agenda/page.jsx
@@ -3,18 +3,17 @@ import { Calendar } from "@/components/ui/calendar";
 import React from "react";
 
 const page = () => {
-  const [date, setDate] = React.useState(new Date());
   return (
     <>
       <h1>AGENDA</h1>
-      <div className="w-auto">
+      {/* <div className="w-auto">
         <Calendar
           mode="single"
           selected={date}
           onSelect={setDate}
           className="rounded-md border"
         />
-      </div>
+      </div> */}
     </>
   );
 };
