7b9bdd2b4f456e98f182c7a509abf83f8c111e8c
Kristien De Boeck <deboeck_kristien@hotmail.com>
2024-05-25 14:36:09 +0200

    dynamic pages

--- src/app/test/page.js
+++ src/app/test/page.js
@@ -1,18 +1,12 @@
 "use client";
 
-import { useRouter } from "next/router";
-
 import Chapters from "@/components/Chapters";
 import Levels from "@/components/Levels";
 
 const page = () => {
-  const router = useRouter();
-  // const { slug } = router.query;
-
   return (
     <div>
       <h1>Test Page</h1>
-      {/* <p>Slug: {slug}</p> */}
     </div>
   );
 };
